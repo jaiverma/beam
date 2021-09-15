309c61bc470391e57e4a9b842e559ee854a02440
h4ven <me.h4ven@gmail.com>
2021-09-15 23:12:42 +0530

    Refactoring dispatch command types

--- beam-ocaml/lib/dispatch.ml
+++ beam-ocaml/lib/dispatch.ml
@@ -1,31 +1,12 @@
 (* responsible for reading messages and dispatching to the appropriate function *)
 
 type cmd =
-  | READ_DIR
-  | READ_FILE
-  | WRITE_FILE
+  | READ_DIR of string
+  | READ_FILE of string
+  | WRITE_FILE of string * string
 
-let call fn args =
-  match fn with
-  | READ_DIR ->
-    let dirname =
-      match args with
-      | [ d ] -> d
-      | _ -> raise (Invalid_argument "READ_DIR")
-    in
-    Ops.read_dir dirname
-  | READ_FILE ->
-    let filename =
-      match args with
-      | [ f ] -> f
-      | _ -> raise (Invalid_argument "READ_FILE")
-    in
-    Ops.read_file filename
-  | WRITE_FILE ->
-    let filename, contents =
-      match args with
-      | [ f; c ] -> f, c
-      | _ -> raise (Invalid_argument "WRITE_FILE")
-    in
-    Ops.write_file filename contents
+let call = function
+  | READ_DIR dirname -> Ops.read_dir dirname
+  | READ_FILE filename -> Ops.read_file filename
+  | WRITE_FILE (filename, contents) -> Ops.write_file filename contents
 ;;
